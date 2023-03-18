9ff413f3f7e71e8c3646127109bb0fab1623585e
Reva Alexander <123241045+Xelarq@users.noreply.github.com>
2023-03-18 10:28:09 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
 import './main.css'
 import './scss/main.scss'
 import './css/reset.css'
-import './libs/gsap/gsap.min.js'
-import './libs/gsap/ScrollTrigger.min.js'
-import './libs/gsap/ScrollSmoother.min.js'
